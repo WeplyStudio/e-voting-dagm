e455d89976f1ae289dcfe6ed016adb0a0ad38b01
Jaysss <matchboxdevelopment@gmail.com>
2026-01-11 05:47:39 +0000

    MongoServerSelectionError: 80C82E55557F0000:error:0A000438:SSL routines:

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -9,19 +9,21 @@ if (!uri) {
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-declare const global: {
-  _mongoClientPromise?: Promise<MongoClient>;
-};
-
-if (process.env.NODE_ENV === 'development') {
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, { tls: true });
-    global._mongoClientPromise = client.connect();
-  }
-  clientPromise = global._mongoClientPromise;
-} else {
-  client = new MongoClient(uri, { tls: true });
+// In production mode, it's best to not use a global variable.
+if (process.env.NODE_ENV === 'production') {
+  client = new MongoClient(uri);
   clientPromise = client.connect();
+} else {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  let globalWithMongo = global as typeof global & {
+    _mongoClientPromise?: Promise<MongoClient>
+  }
+  if (!globalWithMongo._mongoClientPromise) {
+    client = new MongoClient(uri);
+    globalWithMongo._mongoClientPromise = client.connect()
+  }
+  clientPromise = globalWithMongo._mongoClientPromise
 }
 
 export async function getDb() {
